3416ff1bc3f6ef776906aa8af292ce47ea7cbf6e
Mark Soderquist <mark@soderquist.net>
2011-01-01 18:13:38 +0000

    Add Escape utility and service to download page.

---
+++ source/test/java/com/parallelsymmetry/site/MavenDownloadTest.java
@@ -0,0 +1,24 @@
+package com.parallelsymmetry.site;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+public class MavenDownloadTest extends TestCase {
+
+	public void testGetEscapeServiceDownloads() throws Exception {
+		List<MavenDownload> escapeServiceDownloads = MavenDownload.getDownloads( "http://mvn.parallelsymmetry.com/content/groups/psm/com/parallelsymmetry/escape/service" );
+		assertTrue( escapeServiceDownloads.size() > 0 );
+	}
+
+	public void testGetEscapeUtilityDownloads() throws Exception {
+		List<MavenDownload> escapeUtilityDownloads = MavenDownload.getDownloads( "http://mvn.parallelsymmetry.com/content/groups/psm/com/parallelsymmetry/escape/utility" );
+		assertTrue( escapeUtilityDownloads.size() > 0 );
+	}
+	
+	public void testGetTerraceDownloads() throws Exception {
+		List<MavenDownload> terraceDownloads = MavenDownload.getDownloads( "http://mvn.parallelsymmetry.com/content/groups/psm/com/parallelsymmetry/terrace" );
+		assertTrue( terraceDownloads.size() > 0 );
+	}
+
+}
