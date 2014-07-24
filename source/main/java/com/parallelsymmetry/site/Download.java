fa25bf95dcf5ab6ac79f26da51321399e7cce0a0
Mark Soderquist <mark@soderquist.net>
2014-07-24 10:40:08 -0600

    Enhance download stream

--- source/main/java/com/parallelsymmetry/site/Download.java
+++ source/main/java/com/parallelsymmetry/site/Download.java
@@ -18,27 +18,21 @@ public class Download extends HttpServlet {
 
 	private static final long serialVersionUID = 549372381756415485L;
 
-	private static final int CONNECT_TIMEOUT = 5000;
+	private static final int CONNECT_RETRY = 3;
+
+	private static final int CONNECT_TIMEOUT = 4500;
 
 	private static final int READ_TIMEOUT = 1000;
 
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 		String url = null;
-		String source = request.getParameter( "source" );
 		String maven = request.getParameter( "maven" );
+		String source = request.getParameter( "source" );
 
 		if( source != null ) {
 			url = source;
 		} else if( maven != null ) {
-			String classifier = request.getParameter( "classifier" );
-			String type = request.getParameter( "type" );
-			List<MavenDownload> downloads;
-			try {
-				downloads = MavenDownload.getDownloads( maven, classifier, type );
-				if( downloads.size() > 0 ) url = downloads.get( 0 ).getLink();
-			} catch( Exception e ) {
-				getServletContext().log( null, e );
-			}
+			url = getMavenUrl( request, maven );
 		}
 
 		if( url == null ) {
@@ -46,22 +40,33 @@ public class Download extends HttpServlet {
 			return;
 		}
 
-		URL requestUrl = new URL( request.getRequestURL().toString() );
-		String path = requestUrl.getPath();
+		String path = new URL( request.getRequestURL().toString() ).getPath();
 		String filename = path.substring( path.lastIndexOf( "/" ) + 1 );
 
 		stream( response, url, filename );
 	}
 
 	private void stream( HttpServletResponse response, String source, String filename ) throws MalformedURLException, IOException {
-		// Establish the URL connection.
-		URL url = new URL( source );
-		URLConnection connection = url.openConnection();
-		connection.setConnectTimeout( CONNECT_TIMEOUT );
-		connection.setReadTimeout( READ_TIMEOUT );
+		int attempt = 0;
+		InputStream input = null;
+		URLConnection connection = null;
 
-		// Obtain the input stream.
-		InputStream input = connection.getInputStream();
+		URL url = new URL( source );
+		int connectTimeout = CONNECT_TIMEOUT / CONNECT_RETRY;
+
+		while( attempt < CONNECT_RETRY && input == null ) {
+			// Increment the attempt count.
+			attempt++;
+
+			// Establish the connection.
+			connection = url.openConnection();
+			connection.setConnectTimeout( connectTimeout );
+			connection.setReadTimeout( READ_TIMEOUT );
+
+			// Obtain the input stream.
+			input = connection.getInputStream();
+		}
+
 		if( input == null ) {
 			response.sendError( HttpServletResponse.SC_NOT_FOUND );
 			return;
@@ -89,6 +94,23 @@ public class Download extends HttpServlet {
 		}
 		output.close();
 		input.close();
+	}
+
+	private String getMavenUrl( HttpServletRequest request, String maven ) {
+		String url = null;
+	
+		String classifier = request.getParameter( "classifier" );
+		String type = request.getParameter( "type" );
+		List<MavenDownload> downloads;
+	
+		try {
+			downloads = MavenDownload.getDownloads( maven, classifier, type );
+			if( downloads.size() > 0 ) url = downloads.get( 0 ).getLink();
+		} catch( Exception exception ) {
+			getServletContext().log( null, exception );
+		}
+	
+		return url;
 	}
 
 }
