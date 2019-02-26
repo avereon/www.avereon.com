6410fb07389d061ed35fa7de55a11d162cf918c8
Mark Soderquist <mark@soderquist.net>
2019-02-25 20:21:58 -0700

    Updated download content key

--- source/main/java/com/xeomar/web/root/AbstractDownloadProvider.java
+++ source/main/java/com/xeomar/web/root/AbstractDownloadProvider.java
@@ -5,10 +5,10 @@ public abstract class AbstractDownloadProvider implements DownloadProvider {
 	protected String getDownloadKey( String artifact, String category, String type, String channel, String platform ) {
 		StringBuilder builder = new StringBuilder( artifact );
 
-		if( category != null ) builder.append( "-" ).append( category );
-		if( type != null ) builder.append( "-" ).append( type );
 		if( channel != null ) builder.append( "-" ).append( channel );
 		if( platform != null ) builder.append( "-" ).append( platform );
+		if( category != null ) builder.append( "-" ).append( category );
+		if( type != null ) builder.append( "-" ).append( type );
 
 		return builder.toString();
 	}
