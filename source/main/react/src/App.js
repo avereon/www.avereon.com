3334d4d80504bd702eb739c8b688f4f73f880164
Mark Soderquist <soderquistmv@ldschurch.org>
2019-04-26 01:47:12 -0600

    Addition of temporary logo

--- source/main/react/src/App.js
+++ source/main/react/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 
+const ICON_URL="https://www.xeomar.com/download?channel=latest&artifact=mouse&category=product&type=icon";
+
 function App() {
-  return (
-    <div className="App">
-        <h1>Xeomar</h1>
-        <p>Specialized software for specialized work</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1><img className="logo" alt="O" src={ICON_URL}/>Xeomar</h1>
+			<p>Specialized software for specialized work</p>
+		</div>
+	);
 }
 
 export default App;
