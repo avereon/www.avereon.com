411ebd729eb961ce389fed922a8d101ff486d234
Mark Soderquist <soderquistmv@ldschurch.org>
2019-04-25 19:05:54 -0600

    Continue work on react content

--- source/main/java/com/xeomar/web/root/SiteController.java
+++ source/main/java/com/xeomar/web/root/SiteController.java
@@ -1,15 +1,13 @@
 package com.xeomar.web.root;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class SiteController {
 
-	@RequestMapping( "/" )
-	public String index() {
-		return "index.html";
-	}
+//	@RequestMapping( "/" )
+//	public String index() {
+//		return "index.html";
+//	}
 
 }
