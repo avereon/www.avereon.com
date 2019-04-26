60e079fd69e896c100917e3799d3b05442fce839
Mark Soderquist <soderquistmv@ldschurch.org>
2019-04-26 08:48:48 -0600

    Update logo url

--- source/main/react/src/App.js
+++ source/main/react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const ICON_URL="https://www.xeomar.com/download?channel=latest&artifact=mouse&category=product&type=icon";
+const ICON_URL="https://www.xeomar.com/download?channel=latest&artifact=xeomar&category=product&type=icon";
 
 function App() {
 	return (
