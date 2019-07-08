618db51252f03d4c2335e1570bea35a6216541a2
Mark Soderquist <soderquistmv@ldschurch.org>
2019-07-08 07:35:37 -0600

    Add Xenon installer download links

--- source/main/react/src/App.js
+++ source/main/react/src/App.js
@@ -8,6 +8,8 @@ function App() {
 		<div className="App">
 			<h1><img className="logo" alt="O" src={ICON_URL}/>Xeomar</h1>
 			<p>Specialized products for specialized work</p>
+			<a href={'https://www.xeomar.com/download/stable/v2/xenon/linux/install/jar'}>Xenon Installer - Stable Release</a>
+			<a href={'https://www.xeomar.com/download/latest/v2/xenon/linux/install/jar'}>Xenon Installer - Nightly Release</a>
 		</div>
 	);
 }
