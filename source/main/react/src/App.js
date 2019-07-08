352d40558d726ef5108d47b6b5e6d3d9e662e9d8
Mark Soderquist <soderquistmv@ldschurch.org>
2019-07-08 08:33:25 -0600

    Add Xenon installer download links

--- source/main/react/src/App.js
+++ source/main/react/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Platform} from "./Platform";
 
 const ICON_URL="https://www.xeomar.com/download?channel=latest&artifact=xeomar&category=product&type=icon";
 
 function App() {
+
 	return (
 		<div className="App">
-			<h1><img className="logo" alt="O" src={ICON_URL}/>Xeomar</h1>
+			<h1><img className="logo" alt="" src={ICON_URL}/>Xeomar</h1>
 			<p>Specialized products for specialized work</p>
-			<a href={'https://www.xeomar.com/download/stable/v2/xenon/linux/install/jar'}>Xenon Installer - Stable Release</a>
-			<a href={'https://www.xeomar.com/download/latest/v2/xenon/linux/install/jar'}>Xenon Installer - Nightly Release</a>
+			<a href={'https://www.xeomar.com/download/stable/v2/xenon/' + Platform.PLATFORM +'/install/jar'}>Xenon Installer - Stable Release</a>
+			<a href={'https://www.xeomar.com/download/latest/v2/xenon/' + Platform.PLATFORM +'/install/jar'}>Xenon Installer - Nightly Release</a>
 		</div>
 	);
 }
---
+++ source/main/react/src/Platform.js
@@ -0,0 +1,21 @@
+export class Platform {
+
+	static PLATFORM = 'unknown';
+
+	static NAME = 'Unknown';
+
+}
+
+console.log(window.navigator);
+
+let platform = window.navigator.platform.toLocaleLowerCase();
+if (platform.startsWith('linux')) {
+	Platform.PLATFORM = 'linux';
+	Platform.NAME = 'Linux';
+} else if (platform.startsWith('win')) {
+	Platform.PLATFORM = 'windows';
+	Platform.NAME = 'Windows';
+} else if (platform.startsWith('mac')) {
+	Platform.PLATFORM = 'macos';
+	Platform.NAME = 'Mac OS'
+}
