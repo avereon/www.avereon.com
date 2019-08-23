795cbdbddf65a486ae7a815f0b45f0bbc6670d3e
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-23 15:10:20 -0600

    More work on header menu

--- source/main/react/src/Header.js
+++ source/main/react/src/Header.js
@@ -6,13 +6,13 @@ export default class Header extends React.Component {
 	render() {
 		return (
 			<div className='header'>
-				<a className='banner' href='/'><img className='logo' alt="" src={Icon.AVEREON}/>Avereon</a>
+				<a className='banner' href='/'><img className='logo' alt="" src={Icon.AVEREON}/><span>Avereon</span></a>
 				<span className='menu'>
 					<a className='menuitem products' href='/products'>Products</a>
 					<a className='menuitem docs' href='/docs'>Documentation</a>
 					<a className='menuitem support' href='/support'>Support</a>
 					<a className='menuitem about' href='/about'>About</a>
-					</span>
+				</span>
 			</div>
 		)
 	}
