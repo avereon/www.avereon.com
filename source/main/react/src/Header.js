b7547f5fcb12390457567f4936e72e37d4d160b9
Mark Soderquist <mark@soderquist.net>
2019-08-25 20:16:22 -0600

    Cleanup header for release

--- source/main/react/src/Header.js
+++ source/main/react/src/Header.js
@@ -7,12 +7,14 @@ export default class Header extends React.Component {
 		return (
 			<div className='header'>
 				<a className='banner' href='/'><img className='logo' alt="" src={Icon.AVEREON}/><span>Avereon</span></a>
+				{/*
 				<span className='menu'>
 					<a className='menuitem products' href='/products'>Products</a>
 					<a className='menuitem docs' href='/docs'>Documentation</a>
 					<a className='menuitem support' href='/support'>Support</a>
 					<a className='menuitem about' href='/about'>About</a>
 				</span>
+				*/}
 			</div>
 		)
 	}
