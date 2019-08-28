d487baad9aa56baea59f589748210c47b7f6f765
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-28 13:09:05 -0600

    Work with GitHub Actions

--- .github/build.sh
+++ .github/build.sh
@@ -7,7 +7,8 @@ mkdir ${HOME}/.ssh
 echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/rsa_id
 echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/rsa_id.pub
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
-chmod 600 ${HOME}/.ssh/*
+chmod -R 600 ${HOME}/.ssh
+
 #scp -B target/*.jar travis@avereon.com:/opt/avn/store/latest/www.avereon.com
 #scp -B target/main/images/avereon.png travis@avereon.com:/opt/avn/store/stable/avereon/provider.png
 
