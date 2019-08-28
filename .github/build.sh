f3a4e6eaf7f0a075931113b8374d1fa44b021821
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-28 13:07:25 -0600

    Work with GitHub Actions

--- .github/build.sh
+++ .github/build.sh
@@ -4,10 +4,10 @@
 #mvn package -B -U -V --settings .github/settings.xml --file pom.xml
 
 mkdir ${HOME}/.ssh
-#chown 600 ${HOME}/.ssh
 echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/rsa_id
 echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/rsa_id.pub
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
+chown 600 ${HOME}/.ssh/*
 #scp -B target/*.jar travis@avereon.com:/opt/avn/store/latest/www.avereon.com
 #scp -B target/main/images/avereon.png travis@avereon.com:/opt/avn/store/stable/avereon/provider.png
 
