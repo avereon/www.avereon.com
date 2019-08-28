05b756f09e7dfc98b92d2bb98378e800b4f9ce74
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-28 13:35:54 -0600

    Work with GitHub Actions

--- .github/build.sh
+++ .github/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#Xvfb ${DISPLAY} -screen 0 1920x1080x24 -nolisten unix &
-#mvn package -B -U -V --settings .github/settings.xml --file pom.xml
+Xvfb ${DISPLAY} -screen 0 1920x1080x24 -nolisten unix &
+mvn package -B -U -V --settings .github/settings.xml --file pom.xml
 
 mkdir ${HOME}/.ssh
-#chmod 600 ${HOME}/.ssh
 echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/id_rsa
 echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/id_rsa.pub
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
@@ -12,9 +11,5 @@ chmod 600 ${HOME}/.ssh/id_rsa
 chmod 600 ${HOME}/.ssh/id_rsa.pub
 chmod 600 ${HOME}/.ssh/known_hosts
 
-#scp -B target/*.jar travis@avereon.com:/opt/avn/store/latest/www.avereon.com
-#scp -B target/main/images/avereon.png travis@avereon.com:/opt/avn/store/stable/avereon/provider.png
-
-ls -al ${HOME}/.ssh
-echo 'test' > test.txt
-scp -vv -B test.txt travis@avereon.com:test.txt
+scp -B target/*.jar travis@avereon.com:/opt/avn/store/latest/www.avereon.com
+scp -B target/main/images/avereon.png travis@avereon.com:/opt/avn/store/stable/avereon/provider.png
