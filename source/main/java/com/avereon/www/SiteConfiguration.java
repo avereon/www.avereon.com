1204aa8ab23bf8c2892814d48526ba28160428a3
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-30 12:39:41 -0600

    Get ReactRouter working with spring boot hosted content

--- source/main/java/com/avereon/www/SiteConfiguration.java
+++ source/main/java/com/avereon/www/SiteConfiguration.java
@@ -1,8 +1,18 @@
 package com.avereon.www;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class SiteConfiguration {
+public class SiteConfiguration extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers( ViewControllerRegistry registry ) {
+		// A @Controller must be defined for this configuration to work
+		registry.addViewController( "/{spring:\\w+}" ).setViewName( "forward:/index.html" );
+		registry.addViewController( "/**/{spring:\\w+}" ).setViewName( "forward:/index.html" );
+		registry.addViewController( "/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}" ).setViewName( "forward:/index.html" );
+	}
 
 }
--- source/main/java/com/avereon/www/SiteController.java
+++ source/main/java/com/avereon/www/SiteController.java
@@ -2,29 +2,13 @@ package com.avereon.www;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
-import java.nio.charset.StandardCharsets;
 
-//@Controller
+@Controller
 public class SiteController {
 
 	private final Logger log = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
-
-	@RequestMapping( "/" )
-	public InputStream index() {
-		log.warn( "Getting index page" );
-		//return getClass().getClassLoader().getResourceAsStream( "/avnweb/index.html" );
-		return new ByteArrayInputStream( "Index page".getBytes( StandardCharsets.UTF_8 ) );
-	}
-
-//		@RequestMapping( "/{path}" )
-//	public InputStream index( @PathVariable String path ) {
-//		log.warn( "Getting " + path );
-//		return getClass().getResourceAsStream( "/avnweb/" + path );
-//	}
 
 }
