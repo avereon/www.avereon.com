a270ba16852b3363bd91e59bd0979aafe675a56c
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-11-26 10:30:16 -0700

    Improved menu structure

--- client/src/Header.js
+++ client/src/Header.js
@@ -6,16 +6,13 @@ export default class Header extends React.Component {
 	render() {
 		return (
 			<div className='header'>
-				<div className='hbox'>
-					<div>
-						<a className='banner' href='/'><img className='logo' alt="" src={Icon.AVEREON}/>Avereon</a>
-					</div>
-					<div className='menu'>
-						<a className='menuitem products' href='/product'>Products</a>
-					</div>
-				</div>
 				<div className='menu'>
-					<a className='menuitem about' href='/about'>About</a>
+					<a className='banner' href='/'><img className='logo' alt="" src={Icon.AVEREON}/>Avereon</a>
+					<a className='products' href='/product'>Products</a>
+				</div>
+
+				<div className='menu'>
+					<a className='about' href='/about'>About</a>
 				</div>
 			</div>
 		)
