f97f806bc8f758393292a8e923a1d80a117b495c
Mark Soderquist <mark@soderquist.net>
2019-11-27 18:59:24 -0700

    Add some larger screenshots

--- client/src/About.js
+++ client/src/About.js
@@ -4,39 +4,52 @@ import * as Icon from "./Icon";
 export default class About extends React.Component {
 
 	render() {
-		let flow={float: 'left'};
-
 		return (
 			<div className='content'>
 				<h1>About Us</h1>
-				<p>
-					Avereon is a small organization of engineers, students, hobbyists and
-					artists with the simple goal of creating unique products for the work
-					they do.
-				</p>
+				<div className='product-bar'>
+					<img className="product-icon" alt="" src={Icon.AVEREON}/>
+					<div className='body'>
+						<div>
+							We are a small organization of engineers, students, hobbyists and
+							artists with the need to create unique products for the work we
+							do. We hope others will find our tools useful for their own work.
+						</div>
+					</div>
+				</div>
 
 				<h2>Icons</h2>
-				<p>
-					<img className="product-icon" style={flow} alt="" src={Icon.AVEREON}/>
-					The Avereon wing icon is a stylized <a href='https://en.wikipedia.org/wiki/Flying_wing'>flying wing</a> with
-					an advanced	propulsion system, possibly a
-					high-thrust <a href='https://en.wikipedia.org/wiki/Ion_thruster'>ion drive</a>.
-					Several flying wing aircraft used for inspiration include
-					the	<a href='https://en.wikipedia.org/wiki/Northrop_Grumman_B-2_Spirit'>B-2 Spirt</a>,
-					the <a href='https://en.wikipedia.org/wiki/Lockheed_F-117_Nighthawk'>F-117 Nighthawk</a> and
-					the <a href='https://en.wikipedia.org/wiki/Northrop_N-9M'>Northrop N-9M</a>.
-				</p>
-				<p>
-					<img className="product-icon" style={flow} alt="" src={Icon.XENON}/>
-					The Xenon X icon is a stylized <a href='https://en.wikipedia.org/wiki/Bipolar_nebula'>bipolar planetary nebula</a> surrounded
-					by an accretion disk that causes the gas to stay in a double cone-like
-					structure similar to the <a href='https://en.wikipedia.org/wiki/NGC_6302'>Butterfly Nebula</a> or
-					the <a href='https://en.wikipedia.org/wiki/Egg_Nebula'>Egg Nebula</a>. It
-					has also been considered to be a
-					binary <a href='https://en.wikipedia.org/wiki/Herbig%E2%80%93Haro_object'>Herbig-Haro object</a> with
-					the stars orbiting such that their axes are perpendicular to each other, even
-					though no such object has been observed that we are aware of.
-				</p>
+
+				<div className='product-bar'>
+					<img className="product-icon" alt="" src={Icon.AVEREON}/>
+					<div className='body'>
+						<div>
+							The Avereon wing icon is a stylized <a href='https://en.wikipedia.org/wiki/Flying_wing'>flying wing</a> with
+							an advanced propulsion system, possibly a
+							high-thrust <a href='https://en.wikipedia.org/wiki/Ion_thruster'>ion drive</a>.
+							Several flying wing aircraft used for inspiration include
+							the <a href='https://en.wikipedia.org/wiki/Northrop_Grumman_B-2_Spirit'>B-2 Spirt</a>,
+							the <a href='https://en.wikipedia.org/wiki/Lockheed_F-117_Nighthawk'>F-117 Nighthawk</a> and
+							the <a href='https://en.wikipedia.org/wiki/Northrop_N-9M'>Northrop N-9M</a>.
+						</div>
+					</div>
+				</div>
+
+				<div className='product-bar'>
+					<img className="product-icon" alt="" src={Icon.XENON}/>
+					<div className="body">
+						<div>
+							The Xenon X icon is a stylized <a href='https://en.wikipedia.org/wiki/Bipolar_nebula'>bipolar planetary nebula</a> surrounded
+							by an accretion disk that causes the gas to stay in a double cone-like
+							structure similar to the <a href='https://en.wikipedia.org/wiki/NGC_6302'>Butterfly Nebula</a> or
+							the <a href='https://en.wikipedia.org/wiki/Egg_Nebula'>Egg Nebula</a>. It
+							has also been considered to be a
+							binary <a href='https://en.wikipedia.org/wiki/Herbig%E2%80%93Haro_object'>Herbig-Haro object</a> with
+							the stars orbiting such that their axes are perpendicular to each other, even
+							though no such object has been observed that we are aware of.
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
--- client/src/Home.js
+++ client/src/Home.js
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
 		return (
 			<div className='content'>
 				<div className='announcement'>
-					<a href='/product/xenon'><img className="product-icon" alt="" src={Icon.XENON}/></a>
+					<img className="product-icon" alt="" src={Icon.XENON}/>
 					<div className='body'>
 						<div className='title'>
 							Xenon 1.0 Arriving Soon
@@ -17,6 +17,7 @@ export default class Home extends React.Component {
 						</div>
 					</div>
 				</div>
+				<img src="/screenshots/welcome-tool.png" srcSet="/screenshots/welcome-tool@2x.png 2x, /screenshots/welcome-tool.png" alt="Xenon Welcome Tool"/>
 			</div>
 		)
 	}
