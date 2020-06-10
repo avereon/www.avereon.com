98859de2554f43e601a1314cf651725d711d2699
Mark Soderquist <mark@soderquist.net>
2020-06-09 21:37:01 -0600

    Add theme query parameter support

--- server/source/test/java/com/avereon/www/download/V2DownloadTest.java
+++ server/source/test/java/com/avereon/www/download/V2DownloadTest.java
@@ -13,7 +13,18 @@ public class V2DownloadTest {
 	@Test
 	public void testFilename() {
 		assertThat( V2Download.filename( "artifact", "platform", "asset", "format", Map.of() ), is( "artifact-asset-platform.format" ) );
+		assertThat( V2Download.filename( null, "platform", "asset", "format", Map.of() ), is( "asset-platform.format" ) );
 		assertThat( V2Download.filename( null, null, "asset", "format", Map.of() ), is( "asset.format" ) );
+		assertThat( V2Download.filename( "artifact", null, "asset", "format", Map.of() ), is( "artifact-asset.format" ) );
+
+		assertThat( V2Download.filename( "artifact", "platform", "asset", "format", Map.of( "theme", "grey") ), is( "artifact-asset-grey-platform.format" ) );
+		assertThat( V2Download.filename( null, "platform", "asset", "format", Map.of("theme", "grey") ), is( "asset-grey-platform.format" ) );
+		assertThat( V2Download.filename( null, null, "asset", "format", Map.of("theme", "grey") ), is( "asset-grey.format" ) );
+		assertThat( V2Download.filename( "artifact", null, "asset", "format", Map.of("theme", "grey") ), is( "artifact-asset-grey.format" ) );
+
+		// Errata
+		assertThat( V2Download.filename( "artifact", "platform", null, "format", Map.of() ), is( "artifact--platform.format" ) );
+		assertThat( V2Download.filename( "artifact", "platform", "asset", null, Map.of() ), is( "artifact-asset-platform.null" ) );
 	}
 
 }
