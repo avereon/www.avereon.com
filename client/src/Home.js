e49158d7c236e74261b9a09a4174bbe53e26621f
Mark Soderquist <mark@soderquist.net>
2021-05-28 21:29:04 -0600

    Update the Xenon release announcement

--- client/src/Home.js
+++ client/src/Home.js
@@ -18,15 +18,15 @@ export default class Home extends React.Component {
 
 					<div className='body'>
 						<div className='title'>
-							Xenon 1.3 Released
+							Xenon 1.4
 						</div>
 						<div>
 							<a href='/product/xenon'>Download now</a> to get the latest features:
 							<ul>
-								<li>Java 14</li>
-								<li>Java FX 14</li>
-								<li>Material design flat icons</li>
-								<li>Platform specific installers</li>
+								<li>Tool menus in menubar</li>
+								<li>Tool tools in toolbar</li>
+								<li>Data model enhancements</li>
+								<li>Transactional undo/redo</li>
 							</ul>
 						</div>
 					</div>
