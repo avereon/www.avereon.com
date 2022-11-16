3437ce48871ac495bd4c4dcc69e374a71b6dd103
msoderquist <msoderquist@mastercontrol.com>
2022-11-16 09:11:42 -0700

    Fix screenshot linking

--- client/src/Screenshot.js
+++ client/src/Screenshot.js
@@ -1,51 +1,50 @@
 import Config from "./Config";
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 function X1(name) {
-    return Config.IMAGE_ROOT + "/screenshots/" + name + ".png";
+	return Config.IMAGE_ROOT + "/screenshots/" + name + ".png";
 }
 
 function X2(name) {
-    return Config.IMAGE_ROOT + "/screenshots/" + name + "@2x.png";
+	return Config.IMAGE_ROOT + "/screenshots/" + name + "@2x.png";
 }
 
-export default class Screenshot extends React.Component {
-    /* The original screenshots are 800x500 */
-    width = 80 * 10
-    height = 50 * 10
+export default function Screenshot(props) {
+	/* The original screenshots are 800x500 */
+	let width = 80 * 10
+	let height = 50 * 10
 
-    render() {
-        let title = this.props.location.state.title
-        let path = this.props.location.state.path
-        let link = this.props.location.state.returnLink
+	const location = useLocation();
+	let title = location.state.title
+	let path = location.state.path
+	let link = location.state.returnLink
 
-        return (
-            <div className='content'>
-                <h1>{title}</h1>
-                <Link className='screenshot-tile' to={link}>
-                    <img width={this.width} height={this.height} srcSet={X2(path) + " 2x, " + X1(path) + " 1x"} src={X1(path)} alt={title}/>
-                </Link>
-            </div>
-        )
-    }
+	return (
+		<div className='content'>
+			<h1>{title}</h1>
+			<Link className='screenshot-tile' to={link}>
+				<img width={width} height={height} srcSet={X2(path) + " 2x, " + X1(path) + " 1x"} src={X1(path)} alt={title}/>
+			</Link>
+		</div>
+	)
 }
 
-export class ScreenshotTile extends React.Component {
-    width = 80 * 2
-    height = 50 * 2
+export function ScreenshotTile(props) {
 
-    render() {
-        let title = this.props.title
-        let path = this.props.path
-        let returnLink = window.location.pathname
-        return (
-            <div className='content'>
-                <h6>{title}</h6>
-                <Link className='screenshot-tile' to={{pathname: '/product/screenshot', state: {title, path, returnLink}}}>
-                    <img width={this.width} height={this.height} srcSet={X2(path) + " 2x, " + X1(path) + " 1x"} src={X1(path)} alt={title}/>
-                </Link>
-            </div>
-        )
-    }
+	let width = 80 * 2
+	let height = 50 * 2
+	let title = props.title
+	let path = props.path
+	let returnLink = window.location.pathname
+
+	return (
+		<div className='content'>
+			<h6>{title}</h6>
+			<Link className='screenshot-tile' to='/product/screenshot' state={{title, path, returnLink}}>
+				<img width={width} height={height} srcSet={X2(path) + " 2x, " + X1(path) + " 1x"} src={X1(path)} alt={title}/>
+			</Link>
+		</div>
+	)
+
 }
