a40bc921d9d66b7a928b0daa85fd0d2d34f375aa
Mark Soderquist <msoderquist@mastercontrol.com>
2023-05-30 09:32:42 -0600

    Remove xenon-junit5 project

--- client/src/product/Status.js
+++ client/src/product/Status.js
@@ -27,7 +27,6 @@ export default class Products extends React.Component {
 					<h3>Libraries</h3>
 					<ProductStatus ident='marea' icon={Icon.CURVE} name='Marea'/>
 					<ProductStatus ident='curve' icon={Icon.CURVE} name='Curve'/>
-					<ProductStatus ident='xenon-junit5' icon={Icon.CURVE} name='Xenon JUnit5'/>
 					<ProductStatus ident='zarra' icon={Icon.ZARRA} name='Zarra'/>
 					<ProductStatus ident='zenna' icon={Icon.ZENNA} name='Zenna'/>
 					<ProductStatus ident='zerra' icon={Icon.ZERRA} name='Zerra'/>
